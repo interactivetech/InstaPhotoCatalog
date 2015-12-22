c862645f2211187edc2661a4e57316575502589c
unknown <anmendez@MIA-LT-11258.wfs.com>
2015-12-22 12:44:14 -0500

    update on part 2 of tutorial - view, model, and controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,15 @@
 //app.js angular file
 
-var app = angular.app('appController',[]);
+var app = angular.app('InstaCatalogApp',[]);
+
+app.controller('InstagramListCtrl',function(){
+	
+	$scope.photos=[
+		{'name':'Pic 1',
+		'description':'Me and the new #Whip'},
+		{'name':'Pic with the #bae'},
+		
+	];
+});
 
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,1 +1,15 @@
-//controller js
+//controller jsvar app = angular.app('InstaCatalogApp',[]);
+
+var app=angular.module('InstaCatalogApp',[]);
+//declared controller and registered it in AngularJS module
+app.controller('InstagramListCtrl',function($scope){
+	
+	$scope.pics=[
+		{'name':'Pic 1',
+		'description':'Me and the new #Whip'},
+		{'name':'Pic 2',
+		'description':'Pic with the #bae'},
+		
+	];
+});
+
