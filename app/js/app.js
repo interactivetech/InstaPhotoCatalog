2f12e11ae8c3bdd50650cf2ae8218d219835cd1f
interactivetech <interactivetech1@gmail.com>
2016-01-01 14:56:22 -0500

    Completed part of step 3

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,16 @@
 
 var app = angular.app('InstaCatalogApp',[]);
 
+
 app.controller('InstagramListCtrl',function(){
+
+
 	
 	$scope.photos=[
 		{'name':'Pic 1',
 		'description':'Me and the new #Whip'},
-		{'name':'Pic with the #bae'},
-		
+		{'name':'Pic with the #bae'}
+
 	];
 });
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,15 +1,27 @@
 //controller jsvar app = angular.app('InstaCatalogApp',[]);
 
-var app=angular.module('InstaCatalogApp',[]);
+var app=angular.module('InstaCatalogApp',['ngResource']);
+
+var payload = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=255983170.310dbae.f6f38f0ee1e4446c92734dc6fa2faead&callback=JSON_CALLBACK';
+
 //declared controller and registered it in AngularJS module
-app.controller('InstagramListCtrl',function($scope){
+app.controller('InstagramListCtrl',function($scope,$http){
 	
-	$scope.pics=[
-		{'name':'Pic 1',
-		'description':'Me and the new #Whip'},
-		{'name':'Pic 2',
-		'description':'Pic with the #bae'},
-		
-	];
+	//$scope.pics=[
+	//	{'name':'Pic 1',
+	//	'description':'Me and the new #Whip'},
+	//	{'name':'Pic 2',
+	//	'description':'Pic with the #bae'},
+	//
+	//];
+
+	$scope.pics=[];
+	$http.jsonp(payload).success(function(data) {
+		console.log(data.data);
+
+		console.log(data.data[0]);
+		$scope.pics = data.data;
+	});
+
 });
 
